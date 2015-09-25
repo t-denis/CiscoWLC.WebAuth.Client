439bd0d6acf7d7ac4273c073cb5964e5bb27229e
TDenis <dtimukhin@yandex.ru>
2015-09-25 21:39:36 +0300

    Add button event handler

--- CiscoWLC.WebAuth.Client/MainActivity.cs
+++ CiscoWLC.WebAuth.Client/MainActivity.cs
@@ -1,16 +1,37 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Android.Widget;
 
 namespace CiscoWLC.WebAuth.Client
 {
     [Activity(Label = "CiscoWLC.WebAuth.Client", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button _button;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+            _button = FindViewById<Button>(Resource.Id.button);
+            _button.Click += OnButtonClick;
         }
+
+        
+        protected override void OnStart()
+        {
+            Logger.Init(Console.WriteLine);
+            Logger.Info("Starting");
+            base.OnStart();
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            Logger.Info("Click");
+            Toast.MakeText(this, "Click", ToastLength.Short).Show();
+        }
+
     }
 }
 
