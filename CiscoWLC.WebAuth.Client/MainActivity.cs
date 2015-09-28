dd745e73f0c6e884573ff6f40a480f77948f6c16
TDenis <dtimukhin@yandex.ru>
2015-09-28 22:41:00 +0300

    MainActivity Refactoring

--- CiscoWLC.WebAuth.Client/MainActivity.cs
+++ CiscoWLC.WebAuth.Client/MainActivity.cs
@@ -6,7 +6,6 @@ using System.Security.Cryptography.X509Certificates;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Preferences;
 using Android.Views;
 using Android.Widget;
 using CiscoWLC.WebAuth.Client.Core;
@@ -16,79 +15,36 @@ using CiscoWLC.WebAuth.Client.Settings;
 namespace CiscoWLC.WebAuth.Client
 {
     [Activity(Label = "CiscoWLC.WebAuth.Client", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity, ISharedPreferencesOnSharedPreferenceChangeListener
+    public class MainActivity : Activity
     {
         private Button _button;
         private bool _isBusy;
 
         private MainSettings _settings;
-
-        public MainSettings Settings
-        {
-            get
-            {
-                if (_settings == null)
-                    _settings = MainSettings.GetCurrent(this);
-                return _settings;
-            }
-        }
-
+        
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+            _settings = MainSettings.GetCurrent(this);
+
             Logger.Init(LogMessage);
             Logger.Verbose("Creating MainActivity");
-
+            
             _button = FindViewById<Button>(Resource.Id.button);
             _button.Click += OnButtonClick;
         }
 
         protected override void OnStart()
         {
-            PreferenceManager.GetDefaultSharedPreferences(this).RegisterOnSharedPreferenceChangeListener(this);
-
+            _settings = MainSettings.GetCurrent(this);
             UpdateActivityState();
             Logger.Verbose("Starting MainActivity");
             base.OnStart();
+
         }
-
-        protected override void OnStop()
-        {
-            PreferenceManager.GetDefaultSharedPreferences(this).UnregisterOnSharedPreferenceChangeListener(this);
-            Logger.Verbose("Stopping MainActivity");
-            base.OnStop();
-        }
-
-        private void LogMessage(Severity severity, string message)
-        {
-            Console.WriteLine($"{severity}: {message}");
-
-            var allowedToastLevels = new ToastLevel[] { };
-
-            var toastLevel = Settings.OtherSettings.ToastLevel;
-            switch (severity)
-            {
-                case Severity.Debug:
-                    break;
-                case Severity.Verbose:
-                    allowedToastLevels = new[] { ToastLevel.Verbose };
-                    break;
-                case Severity.Info:
-                    allowedToastLevels = new[] { ToastLevel.Info, ToastLevel.Verbose };
-                    break;
-                case Severity.Warn:
-                case Severity.Error:
-                    allowedToastLevels = new[] { ToastLevel.Info, ToastLevel.Verbose, ToastLevel.WarningsErrorsAndConnected };
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(severity), severity, null);
-            }
-            if (allowedToastLevels.Contains(toastLevel))
-                ShowToast(message);
-        }
-
+        
         private async void OnButtonClick(object sender, EventArgs e)
         {
             if (_button.Text == GetString(Resource.String.Settings))
@@ -105,7 +61,7 @@ namespace CiscoWLC.WebAuth.Client
                     _isBusy = true;
                     _button.Text = GetString(Resource.String.Busy);
                     Logger.Verbose("Connecting");
-                    var settings = Settings;
+                    var settings = _settings;
                     var conManager = new WifiConnector();
                     var ssid = new Ssid(settings.ConnectionSettings.Ssid);
                     await conManager.ConnectAsync(this, ssid,
@@ -131,16 +87,6 @@ namespace CiscoWLC.WebAuth.Client
             }
         }
 
-        private void OpenWebBrowserIfRequired(MainSettings settings)
-        {
-            if (!string.IsNullOrWhiteSpace(settings.OtherSettings.StartUrl))
-            {
-                var uri = Android.Net.Uri.Parse(settings.OtherSettings.StartUrl);
-                var intent = new Intent(Intent.ActionView, uri);
-                StartActivity(intent);
-            }
-        }
-
         #region Menu
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -162,36 +108,81 @@ namespace CiscoWLC.WebAuth.Client
 
         #endregion
 
+        #region Logging
+
+        private void LogMessage(Severity severity, string message)
+        {
+            Console.WriteLine($"{severity}: {message}");
+
+            var allowedToastLevels = new ToastLevel[] { };
+
+            var toastLevel = _settings.OtherSettings.ToastLevel;
+            switch (severity)
+            {
+                case Severity.Debug:
+                    break;
+                case Severity.Verbose:
+                    allowedToastLevels = new[] { ToastLevel.Verbose };
+                    break;
+                case Severity.Info:
+                    allowedToastLevels = new[] { ToastLevel.Info, ToastLevel.Verbose };
+                    break;
+                case Severity.Warn:
+                case Severity.Error:
+                    allowedToastLevels = new[]
+                    {ToastLevel.Info, ToastLevel.Verbose, ToastLevel.WarningsErrorsAndConnected};
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(severity), severity, null);
+            }
+            if (allowedToastLevels.Contains(toastLevel))
+                ShowToast(message);
+        }
+
+        #endregion
+        
+        #region WebBrowser
+
+        private void OpenWebBrowserIfRequired(MainSettings settings)
+        {
+            if (!string.IsNullOrWhiteSpace(settings.OtherSettings.StartUrl))
+            {
+                var uri = Android.Net.Uri.Parse(settings.OtherSettings.StartUrl);
+                var intent = new Intent(Intent.ActionView, uri);
+                StartActivity(intent);
+            }
+        }
+
+        #endregion
+
+        #region Toasts
+
         private void ShowToast(string message)
         {
             Toast.MakeText(this, message, ToastLength.Short).Show();
         }
 
-        private bool AllowInvalidSslCertificates(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        #endregion
+
+        #region SSL
+
+        private bool AllowInvalidSslCertificates(object sender, X509Certificate cert, X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
         {
             Logger.Verbose("Accepting SSL Certificate");
             return true;
         }
 
-        public void OnSharedPreferenceChanged(ISharedPreferences sharedPreferences, string key)
-        {
-            UpdateSettingsAndActivityState();
-        }
-
-        private void UpdateSettingsAndActivityState()
-        {
-            _settings = MainSettings.GetCurrent(this);
-            UpdateActivityState();
-        }
+        #endregion
 
         private void UpdateActivityState()
         {
-            if (Settings.OtherSettings.IgnoreSslCertErrors)
+            if (_settings.OtherSettings.IgnoreSslCertErrors)
                 ServicePointManager.ServerCertificateValidationCallback = AllowInvalidSslCertificates;
             else
                 ServicePointManager.ServerCertificateValidationCallback = null;
 
-            _button.Text = GetString(Settings.AreValid()
+            _button.Text = GetString(_settings.AreValid()
                 ? Resource.String.Connect
                 : Resource.String.Settings);
         }
